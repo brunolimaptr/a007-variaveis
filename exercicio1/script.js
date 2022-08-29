d894c1a56200a43eb28e0c2b5e77aa5026f35ea4
brunolimaptr <brunolima.ptr@gmail.com>
2022-08-29 17:06:45 -0300

    projeto

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -5,7 +5,7 @@ let idade = Number(prompt("Qual sua idade?"))
 
 console.log(nome, typeof nome)
 console.log(idade, typeof idade)
-
-//idade está declarada como script*//
+ 
+//retornou as duas como string, coloquei o number em idade*//
 
 console.log("Olá", nome,"você tem", idade,"anos.")
